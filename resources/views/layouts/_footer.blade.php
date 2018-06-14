c8f0191f8d1cf076623fc626ae6c5d190e737d7b
Jeff <1191881019@qq.com>
2018-06-14 15:41:00 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://weibo.com/u/2979863030" target="_blank">Jeff</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:jcyiqifun@163.com">联系</a></p>
+    </div>
+</footer>
