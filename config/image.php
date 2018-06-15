9d0537f4a075909b5b2a28ed2d6c7133922eb9c4
Jeff <1191881019@qq.com>
2018-06-15 18:45:10 +0800

    User can upload avatar

---
+++ config/image.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Image Driver
+    |--------------------------------------------------------------------------
+    |
+    | Intervention Image supports "GD Library" and "Imagick" to process images
+    | internally. You may choose one of them according to your PHP
+    | configuration. By default PHP's "GD Library" implementation is used.
+    |
+    | Supported: "gd", "imagick"(这个需要比较专业的图片)
+    |
+    */
+
+    'driver' => 'gd'
+
+];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'laraBBS') - Laravel</title>
+    <title>@yield('title', 'LaraBBS') - Laravel</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
