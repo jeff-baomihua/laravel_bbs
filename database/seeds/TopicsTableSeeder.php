f76652b10531b41edc613c6e751f210926b76e39
Jeff <1191881019@qq.com>
2018-07-13 15:44:26 +0800

    Seeding topics and users data

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Category;
+use App\Models\User;
+use Faker\Generator;
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
 
@@ -7,11 +10,24 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
-        });
+        //所有用户ID 数组
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        //所有分类ID 数组
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        //获取faker实例
+        $faker = app(Generator::class);
+
+        $topics = factory(Topic::class)
+                        ->times(100)
+                        ->make()
+                        ->each(function ($topic, $index) use ($user_ids, $category_ids, $faker) {
+                            //从用户ID 数组中随机取出一个并赋值
+                            $topic->user_id = $faker->randomElement($user_ids);
+                            //话题分类 同上
+                            $topic->category_id = $faker->randomElement($category_ids);
+                        });
 
         Topic::insert($topics->toArray());
     }
