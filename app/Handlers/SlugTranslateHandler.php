bfebbafbb32c7ca031a67a8f205138539d5580da
vagrant <vagrant@homestead>
2018-08-08 00:44:26 +0000

    slugTranslate rewrite

--- app/Handlers/SlugTranslateHandler.php
+++ app/Handlers/SlugTranslateHandler.php
@@ -7,40 +7,49 @@ use Overtrue\Pinyin\Pinyin;
 
 class SlugTranslateHandler
 {
+    private $api = 'http://api.fanyi.baidu.com/api/trans/vip/translate?';
+    private $appid;
+    private $key;
+    private $salt;
+    private $from;
+    private $to;
+
+    public function __construct($from='zh', $to='en')
+    {
+        $this->appid = config('services.baidu_translate.appid');
+        $this->key   = config('services.baidu_translate.key');
+        $this->salt  = time();
+        $this->from  = $from;
+        $this->to    = $to;
+    }
+
     public function translate($text)
     {
-        // 实例化 HTTP 客户端
-        $http = new Client;
-
-        // 初始化配置信息
-        $api = 'http://api.fanyi.baidu.com/api/trans/vip/translate?';
-        $appid = config('services.baidu_translate.appid');
-        $key = config('services.baidu_translate.key');
-        $salt = time();
-
         // 如果没有配置百度翻译，自动使用兼容的拼音方案
-        if (empty($appid) || empty($key)) {
+        if (empty($this->appid) || empty($this->key)) {
             return $this->pinyin($text);
         }
+
+        // 实例化 HTTP 客户端
+        $http = new Client;
 
         // 根据文档，生成 sign
         // http://api.fanyi.baidu.com/api/trans/product/apidoc
         // appid+q+salt+密钥 的MD5值
-        $sign = md5($appid. $text . $salt . $key);
+        $sign = md5($this->appid. $text . $this->salt . $this->key);
 
         // 构建请求参数
         $query = http_build_query([
             "q"     =>  $text,
             "from"  => "zh",
             "to"    => "en",
-            "appid" => $appid,
-            "salt"  => $salt,
+            "appid" => $this->appid,
+            "salt"  => $this->salt,
             "sign"  => $sign,
         ]);
 
         // 发送 HTTP Get 请求
-        $response = $http->get($api.$query);
-
+        $response = $http->get($this->api.$query);
 
         $result = json_decode($response->getBody(), true);
 
